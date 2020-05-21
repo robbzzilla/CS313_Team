bfc2ef4a9e1946012cbd6b8400a2f58367bbd627
Robert Hampton <robbzilla@byui.edu>
2020-05-20 20:40:31 -0500

    Changes to index

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+echo '<script src="script.js"></script>';
+echo '<div class="container-fluid">';
+echo '<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-bottom">';
+echo '<ul class="navbar-nav mx-auto">';
+echo '</ul>';
+echo '</nav>';
+echo '</div>';
+?>
--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <html lang="en">
 <body>
 <?php
+    include 'header.php';
     echo '<a href="https://group02.herokuapp.com/week05/scriptures.php">Week 05 Teach';
+    include 'footer.php';
 ?>
 </body>
 </html>
